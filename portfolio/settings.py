1751ca5eb0981c1c3e8312c68a74cf829eeb6abb
gitkp11 <patilkrunal11.kp@gmail.com>
2020-11-18 20:12:26 +0530

    converted main.html into smalller files

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '&cahwqh=j%ky27#js%9ji+^*fp))_b+n=rt2#ohhwt+$g4zaso'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS =  ['*', 'gitkp.azurewebsites.net']
+ALLOWED_HOSTS =  ['gitkp.azurewebsites.net', '.azurewebsites.net', '*']
 
 # Application definition
 
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'portfolio.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
